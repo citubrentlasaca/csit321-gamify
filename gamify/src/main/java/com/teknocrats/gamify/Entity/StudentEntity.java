9d71994df807af54c1905ff292759d6489c51bd9
Ginalyn Caneda <ginalyn.caneda@cit.edu>
2022-11-21 16:41:06 +0800

    Add data members of StudentEntity

---
+++ gamify/src/main/java/com/teknocrats/gamify/Entity/StudentEntity.java
@@ -0,0 +1,12 @@
+package com.teknocrats.gamify.Entity;
+
+public class StudentEntity {
+	private int studentid;
+	private AccountEntity sccount;
+	private Name name;
+	private String gender;
+	private String birthday;
+	private String program;
+	private int yearlevel;
+	private String isdeleted;
+}
