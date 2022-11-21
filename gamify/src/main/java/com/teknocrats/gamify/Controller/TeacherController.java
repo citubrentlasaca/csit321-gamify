35694d5d730e4ffc30d0d73170c1e66065ee90ca
Stephanie Micah Cahutay <stephaniemicah.cahutay@cit.edu>
2022-11-22 00:08:58 +0800

    Implement insertTeacher in TeacherCOntroller

--- gamify/src/main/java/com/teknocrats/gamify/Controller/TeacherController.java
+++ gamify/src/main/java/com/teknocrats/gamify/Controller/TeacherController.java
@@ -4,9 +4,12 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.spcahutay.sims.Entity.StudentEntity;
 import com.teknocrats.gamify.Entity.TeacherEntity;
 import com.teknocrats.gamify.Service.TeacherService;
 
@@ -17,9 +20,14 @@ public class TeacherController {
 	@Autowired
 	TeacherService tserv;
 	
-	//Read all records
-		@GetMapping("/getAllTeachers")
-		public List<TeacherEntity> getAllTeachers(){
-			return tserv.getAllTeachers();
-		}
+	@PostMapping("/postTeacher")
+	public TeacherEntity insertTeacher(@RequestBody TeacherEntity teacher) {
+		return tserv.insertTeacher(teacher);
+	}
+	
+
+	@GetMapping("/getAllTeachers")
+	public List<TeacherEntity> getAllTeachers(){
+		return tserv.getAllTeachers();
+	}
 }
