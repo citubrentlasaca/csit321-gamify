106984fc06879b8e925e122bee7c7116959d8fd4
Stephanie Micah Cahutay <stephaniemicah.cahutay@cit.edu>
2022-11-21 23:52:59 +0800

    Import TEacherRepo in TeacherService

--- gamify/src/main/java/com/teknocrats/gamify/Service/TeacherService.java
+++ gamify/src/main/java/com/teknocrats/gamify/Service/TeacherService.java
@@ -1,8 +1,15 @@
 package com.teknocrats.gamify.Service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.teknocrats.gamify.Entity.TeacherEntity;
+import com.teknocrats.gamify.Repository.TeacherRepository;
+
 
 @Service 
 public class TeacherService {
-
+	@Autowired
+	TeacherRepository trepo; 
+	
 }
