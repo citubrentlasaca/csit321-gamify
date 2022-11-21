56b944392a82aaa957b6bd153847e9d4a349f9e9
Stephanie Micah Cahutay <stephaniemicah.cahutay@cit.edu>
2022-11-21 23:49:14 +0800

    Extend TeacherRepository to JpaRepository

--- gamify/src/main/java/com/teknocrats/gamify/Repository/TeacherRepository.java
+++ gamify/src/main/java/com/teknocrats/gamify/Repository/TeacherRepository.java
@@ -1,8 +1,11 @@
 package com.teknocrats.gamify.Repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.teknocrats.gamify.Entity.TeacherEntity;
+
 @Repository
-public interface TeacherRepository {
+public interface TeacherRepository extends JpaRepository<TeacherEntity, Integer>{
 
 }
