5fb85b4510901103311a6e522d8344ce25bf4dcd
Ginalyn Caneda <ginalyn.caneda@cit.edu>
2022-11-21 16:45:44 +0800

    Add NameEntity

---
+++ gamify/src/main/java/com/teknocrats/gamify/Entity/NameEntity.java
@@ -0,0 +1,29 @@
+package com.teknocrats.gamify.Entity;
+
+public class NameEntity {
+	
+	private String firstname;
+	private String lastname;
+	
+	public NameEntity(String firstname, String lastname) {
+		super();
+		this.firstname = firstname;
+		this.lastname = lastname;
+	}
+
+	public String getFirstname() {
+		return firstname;
+	}
+
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+
+	public String getLastname() {
+		return lastname;
+	}
+
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+}
