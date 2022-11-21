929b1f0e77396d661c9357547794c9d6cdb4c13c
Stephanie Micah Cahutay <stephaniemicah.cahutay@cit.edu>
2022-11-22 00:11:00 +0800

    Implement deleteTeacher in TeacherController

--- gamify/src/main/java/com/teknocrats/gamify/Controller/TeacherController.java
+++ gamify/src/main/java/com/teknocrats/gamify/Controller/TeacherController.java
@@ -3,19 +3,21 @@ package com.teknocrats.gamify.Controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.spcahutay.sims.Entity.StudentEntity;
 import com.teknocrats.gamify.Entity.TeacherEntity;
 import com.teknocrats.gamify.Service.TeacherService;
 
 
 @RestController
 @RequestMapping("/teacher")
+
 public class TeacherController {
 	@Autowired
 	TeacherService tserv;
@@ -29,5 +31,11 @@ public class TeacherController {
 	@GetMapping("/getAllTeachers")
 	public List<TeacherEntity> getAllTeachers(){
 		return tserv.getAllTeachers();
+	}
+	
+	//Delete a record
+	@DeleteMapping("/deleteTeacher/{id}")
+	public String deleteTeacher (@PathVariable int id) {
+		return tserv.deleteTeacher(id);
 	}
 }
