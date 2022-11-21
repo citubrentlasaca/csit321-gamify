0855090a40ccc2f51d09f5d8953e1e001afbba1e
Ginalyn Caneda <ginalyn.caneda@cit.edu>
2022-11-21 23:07:08 +0800

    Implement insertStudent method in StudentController

--- gamify/src/main/java/com/teknocrats/gamify/Controller/StudentController.java
+++ gamify/src/main/java/com/teknocrats/gamify/Controller/StudentController.java
@@ -1,5 +1,23 @@
 package com.teknocrats.gamify.Controller;
 
-public class StudentController {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import com.teknocrats.gamify.Entity.StudentEntity;
+import com.teknocrats.gamify.Service.StudentService;
+
+@RestController
+@RequestMapping("/student")
+public class StudentController {
+	
+	@Autowired
+	StudentService studentService;
+	
+	@PostMapping("/postStudent")
+	public StudentEntity insertStudent(@RequestBody StudentEntity student) {
+		return studentService.insertStudent(student);
+	}
 }
