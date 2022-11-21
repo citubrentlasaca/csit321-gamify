4990b23f9e04044eee35e8ec8804fc24107c1a1d
Ginalyn Caneda <ginalyn.caneda@cit.edu>
2022-11-21 22:52:29 +0800

    Implement insertStudent method in StudentService

--- gamify/src/main/java/com/teknocrats/gamify/Service/StudentService.java
+++ gamify/src/main/java/com/teknocrats/gamify/Service/StudentService.java
@@ -1,5 +1,20 @@
 package com.teknocrats.gamify.Service;
 
-public class StudentService {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import com.teknocrats.gamify.Entity.StudentEntity;
+import com.teknocrats.gamify.Repository.StudentRepository;
+
+@Service
+public class StudentService {
+	
+	@Autowired
+	StudentRepository studentRepository;
+	
+	public StudentEntity insertStudent(StudentEntity student) {
+		return studentRepository.save(student);
+	}
+	
+	
 }
