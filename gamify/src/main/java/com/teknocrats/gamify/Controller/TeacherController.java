1dfc499bf30150f5e09fe1c5a0297bd61f27c4b5
Stephanie Micah Cahutay <stephaniemicah.cahutay@cit.edu>
2022-11-22 00:07:03 +0800

    Implement getAllTeachers in TeacherController

--- gamify/src/main/java/com/teknocrats/gamify/Controller/TeacherController.java
+++ gamify/src/main/java/com/teknocrats/gamify/Controller/TeacherController.java
@@ -1,10 +1,25 @@
 package com.teknocrats.gamify.Controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.teknocrats.gamify.Entity.TeacherEntity;
+import com.teknocrats.gamify.Service.TeacherService;
+
 
 @RestController
 @RequestMapping("/teacher")
 public class TeacherController {
-
+	@Autowired
+	TeacherService tserv;
+	
+	//Read all records
+		@GetMapping("/getAllTeachers")
+		public List<TeacherEntity> getAllTeachers(){
+			return tserv.getAllTeachers();
+		}
 }
