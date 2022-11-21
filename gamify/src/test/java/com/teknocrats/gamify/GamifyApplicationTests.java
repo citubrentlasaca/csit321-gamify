0dd6f33e81898554be20f123814458f6aae8e9ec
Ginalyn Caneda <ginalyn.caneda@cit.edu>
2022-11-21 11:13:10 +0800

    Add gamify folder

---
+++ gamify/src/test/java/com/teknocrats/gamify/GamifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teknocrats.gamify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
