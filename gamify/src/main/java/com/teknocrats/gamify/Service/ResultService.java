5ac4b6eef35be0688ff5456de1b989268a12fbd6
Elaiza Faye Pepito <elaizafaye.pepito@cit.edu>
2022-11-22 12:38:36 +0800

    Add ResultService

---
+++ gamify/src/main/java/com/teknocrats/gamify/Service/ResultService.java
@@ -0,0 +1,11 @@
+package com.teknocrats.gamify.Service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.teknocrats.gamify.Repository.ResultRepository;
+
+public class ResultService {
+	@Autowired
+	ResultRepository resultrepo;
+
+}
