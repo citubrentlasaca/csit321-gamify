1908ee64539c7963bf06f4062dd2162b717e4c32
Ginalyn Caneda <ginalyn.caneda@cit.edu>
2022-11-22 12:06:51 +0800

    Import AccountService

--- gamify/src/main/java/com/teknocrats/gamify/Controller/AccountController.java
+++ gamify/src/main/java/com/teknocrats/gamify/Controller/AccountController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.teknocrats.gamify.Entity.AccountEntity;
+import com.teknocrats.gamify.Service.AccountService;
 
 @RestController
 @RequestMapping("/account")
