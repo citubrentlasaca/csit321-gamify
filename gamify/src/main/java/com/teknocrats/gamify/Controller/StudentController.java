961ce9d2b46146df0ad8d02cf04060080318776c
Ginalyn Caneda <ginalyn.caneda@cit.edu>
2022-11-22 20:05:29 +0800

    Implement deleteStudent method in StudentController

--- gamify/src/main/java/com/teknocrats/gamify/Controller/StudentController.java
+++ gamify/src/main/java/com/teknocrats/gamify/Controller/StudentController.java
@@ -3,7 +3,9 @@ package com.teknocrats.gamify.Controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -34,5 +36,10 @@ public class StudentController {
 	@PutMapping("/putStudent")
 	public StudentEntity putStudent(@RequestParam int studentid, @RequestBody StudentEntity newStudentDetails) throws Exception{
 		return studentService.putStudent(studentid, newStudentDetails);
+	}
+	
+	@DeleteMapping("/deleteStudent/{studentid}")
+	public String deleteStudent(@PathVariable int studentid) {
+		return studentService.deleteStudent(studentid);
 	}
 }
