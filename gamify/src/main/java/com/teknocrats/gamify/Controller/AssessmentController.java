0cb6135651571218b85203e81e7895c931219516
Jonas Angelo Clamor <jonasangelo.clamor@cit.edu>
2022-11-22 17:21:24 +0800

    Implement insertAssessment Method In AssessmentController

--- gamify/src/main/java/com/teknocrats/gamify/Controller/AssessmentController.java
+++ gamify/src/main/java/com/teknocrats/gamify/Controller/AssessmentController.java
@@ -1,5 +1,32 @@
 package com.teknocrats.gamify.Controller;
 
-public class AssessmentController {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.teknocrats.gamify.Entity.AssessmentEntity;
+import com.teknocrats.gamify.Service.AssessmentService;
+
+
+@RestController
+@RequestMapping("/assessment")
+public class AssessmentController {
+    
+    @Autowired
+    AssessmentService assessmentService;
+    
+    //Create or insert an assessment Record
+    @PostMapping("/postAssessment")
+    public AssessmentEntity insertAssessment(@RequestBody AssessmentEntity assessment) {
+    	return assessmentService.insertAssessment(assessment);
+    }
 }
