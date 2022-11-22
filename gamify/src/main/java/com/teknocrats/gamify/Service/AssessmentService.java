9e772dc490f9ddd6227150a665f45a62e9207501
Jonas Angelo Clamor <jonasangelo.clamor@cit.edu>
2022-11-22 17:49:06 +0800

    Edit AssessmentService Class

--- gamify/src/main/java/com/teknocrats/gamify/Service/AssessmentService.java
+++ gamify/src/main/java/com/teknocrats/gamify/Service/AssessmentService.java
@@ -22,58 +22,59 @@ public class AssessmentService {
 	@Autowired
 	AssessmentRepository assessmentRepository;
 	
-		//Create or insert an assessment record 
-		public AssessmentEntity insertAssessment(AssessmentEntity assessment) {
-			return assessmentRepository.save(assessment);
-		}
+	//Create or insert an assessment record 
+	public AssessmentEntity insertAssessment(AssessmentEntity assessment) {
+		return assessmentRepository.save(assessment);
+	}
 		
-		//Read all assessment records from the table named tbl_assessment
-		public List<AssessmentEntity> getAllAssessments(){
-			return assessmentRepository.findAll();
-		}
+	//Read all assessment records from the table named tbl_assessment
+	public List<AssessmentEntity> getAllAssessments(){
+		return assessmentRepository.findAll();
+	}
 		
-		//Read or search an assessment record by title
-		public AssessmentEntity findByTitle(String title) {
-			if(assessmentRepository.findByTitle(title) != null)
-				return assessmentRepository.findByTitle(title);
-			else 
-				return null;
-		}
+	//Read or search an assessment record by title
+	public AssessmentEntity findByTitle(String title) {
+		if(assessmentRepository.findByTitle(title) != null)
+			return assessmentRepository.findByTitle(title);
+		else 
+			return null;
+	}
 		
-		//Update an assessment record
-		public AssessmentEntity putAssessment(int id, AssessmentEntity newAssessmentDetails) throws Exception {
-			AssessmentEntity assessment = new AssessmentEntity();
+	//Update an assessment record
+	public AssessmentEntity putAssessment(int id, AssessmentEntity newAssessmentDetails) throws Exception {
+		AssessmentEntity assessment = new AssessmentEntity();
 					
-				try {
-					//Step 1 - search the id number of the assessment
-					assessment = assessmentRepository.findById(id).get();
+			try {
+				//Step 1 - search the id number of the assessment
+				assessment = assessmentRepository.findById(id).get();
 						
-					//Step 2 - update the title, instructions, description and isdeleted
-					assessment.setTitle(newAssessmentDetails.getTitle());
-					assessment.setInstructions(newAssessmentDetails.getInstructions());
-					assessment.setDescription(newAssessmentDetails.getDescription());
-					assessment.setIsdeleted(newAssessmentDetails.getIsdeleted());
+				//Step 2 - update the title, instructions, description and isdeleted
+				assessment.setTitle(newAssessmentDetails.getTitle());
+				assessment.setInstructions(newAssessmentDetails.getInstructions());
+				assessment.setDescription(newAssessmentDetails.getDescription());
+				assessment.setIsdeleted(newAssessmentDetails.getIsdeleted());
 						
-					//Step 3 - save the info and return its corresponding value 
-					return assessmentRepository.save(assessment);
+				//Step 3 - save the info and return its corresponding value 
+				return assessmentRepository.save(assessment);
 					
-					}catch (NoSuchElementException ex) {
-						throw new Exception("ID number " + id + " does not exist.");
-					}
-		}
+			}
+			catch (NoSuchElementException ex) {
+				throw new Exception("ID number " + id + " does not exist.");
+			}
+	}
 		
-		//Delete an assessment record
-		public String deleteAssessment (int id) {
-			String msg;
+	//Delete an assessment record
+	public String deleteAssessment (int id) {
+		String msg;
 					
-				if(assessmentRepository.findById(id) != null) {
-					assessmentRepository.deleteById(id);
+			if(assessmentRepository.findById(id) != null) {
+				assessmentRepository.deleteById(id);
 						
-					msg = "Assessment with an ID Number " + id + " is successfully deleted!";
-				}
-				else {
-					msg = "Assessment with an ID Number " + id + " is NOT found!";
-				}
-					return msg;
-		}
+				msg = "Assessment with an ID Number " + id + " is successfully deleted!";
+			}
+			else {
+				msg = "Assessment with an ID Number " + id + " is NOT found!";
+			}
+				return msg;
+	}
 }
