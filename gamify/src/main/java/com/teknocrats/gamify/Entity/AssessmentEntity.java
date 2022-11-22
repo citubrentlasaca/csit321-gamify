4443fc47052b1c20c1de01e21e6e5748fa01ff53
Jonas Angelo Clamor <jonasangelo.clamor@cit.edu>
2022-11-22 16:30:35 +0800

    Add Data Memebers Of AssessmentEntity

--- gamify/src/main/java/com/teknocrats/gamify/Entity/AssessmentEntity.java
+++ gamify/src/main/java/com/teknocrats/gamify/Entity/AssessmentEntity.java
@@ -1,5 +1,22 @@
 package com.teknocrats.gamify.Entity;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tbl_assessment", schema ="db_teknocrats_gamify")
 public class AssessmentEntity {
+	
+		@Id
+		@GeneratedValue(strategy = GenerationType.IDENTITY)
+		private int assessmentid;
+		
+		private String title;
+		private String instructions;
+		private String description;
+		private String isdeleted;
 	
 }
