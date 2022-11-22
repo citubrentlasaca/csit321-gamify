145cba15e4df30096ad747feef12d82e639c7d2e
Elaiza Faye Pepito <elaizafaye.pepito@cit.edu>
2022-11-22 12:41:47 +0800

    Implement getAllResult method in ResultService

--- gamify/src/main/java/com/teknocrats/gamify/Service/ResultService.java
+++ gamify/src/main/java/com/teknocrats/gamify/Service/ResultService.java
@@ -1,11 +1,26 @@
 package com.teknocrats.gamify.Service;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.teknocrats.gamify.Entity.ResultEntity;
 import com.teknocrats.gamify.Repository.ResultRepository;
 
+@Service
 public class ResultService {
 	@Autowired
 	ResultRepository resultrepo;
+
+	// C - Create or insert a result record
+	public ResultEntity insertResult(ResultEntity result) {
+		return resultrepo.save(result);
+	}
+
+	// R - Read all records from the table named tbl_result
+	public List<ResultEntity> getAllResult(){
+		return resultrepo.findAll();
+	}
 
 }
