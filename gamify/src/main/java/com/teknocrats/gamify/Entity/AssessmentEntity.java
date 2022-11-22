e429d00164d4e6fbfdf5632b6f581ca1ca79ee56
Jonas Angelo Clamor <jonasangelo.clamor@cit.edu>
2022-11-22 17:42:30 +0800

    Edit AssessmentEntity Class

--- gamify/src/main/java/com/teknocrats/gamify/Entity/AssessmentEntity.java
+++ gamify/src/main/java/com/teknocrats/gamify/Entity/AssessmentEntity.java
@@ -10,63 +10,63 @@ import javax.persistence.Table;
 @Table(name = "tbl_assessment", schema ="db_teknocrats_gamify")
 public class AssessmentEntity {
 	
-		@Id
-		@GeneratedValue(strategy = GenerationType.IDENTITY)
-		private int assessmentid;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int assessmentid;
 		
-		private String title;
-		private String instructions;
-		private String description;
-		private String isdeleted;
+	private String title;
+	private String instructions;
+	private String description;
+	private String isdeleted;
 		
-		public AssessmentEntity () {}
+	public AssessmentEntity () {}
 
-		public AssessmentEntity(int assessmentid, String title, String instructions, String description, String isdeleted) {
-			super();
-			this.assessmentid = assessmentid;
-			this.title = title;
-			this.instructions = instructions;
-			this.description = description;
-			this.isdeleted = isdeleted;
-		}
+	public AssessmentEntity(int assessmentid, String title, String instructions, String description, String isdeleted) {
+		super();
+		this.assessmentid = assessmentid;
+		this.title = title;
+		this.instructions = instructions;
+		this.description = description;
+		this.isdeleted = isdeleted;
+	}
 		
-		public int getAssessmentid() {
-			return assessmentid;
-		}
+	public int getAssessmentid() {
+		return assessmentid;
+	}
 
-		public void setAssessmentid(int assessmentid) {
-			this.assessmentid = assessmentid;
-		}
+	public void setAssessmentid(int assessmentid) {
+		this.assessmentid = assessmentid;
+	}
 
-		public String getTitle() {
-			return title;
-		}
+	public String getTitle() {
+		return title;
+	}
 
-		public void setTitle(String title) {
-			this.title = title;
-		}
+	public void setTitle(String title) {
+		this.title = title;
+	}
 
-		public String getInstructions() {
-			return instructions;
-		}
+	public String getInstructions() {
+		return instructions;
+	}
 
-		public void setInstructions(String instructions) {
-			this.instructions = instructions;
-		}
+	public void setInstructions(String instructions) {
+		this.instructions = instructions;
+	}
 
-		public String getDescription() {
-			return description;
-		}
+	public String getDescription() {
+		return description;
+	}
 
-		public void setDescription(String description) {
-			this.description = description;
-		}
+	public void setDescription(String description) {
+		this.description = description;
+	}
 		
-		public String getIsdeleted() {
-			return isdeleted;
-		}
+	public String getIsdeleted() {
+		return isdeleted;
+	}
 
-		public void setIsdeleted(String isdeleted) {
-			this.isdeleted = isdeleted;
-		}
+	public void setIsdeleted(String isdeleted) {
+		this.isdeleted = isdeleted;
+	}	
 }
