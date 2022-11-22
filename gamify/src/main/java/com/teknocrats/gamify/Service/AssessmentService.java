4b8635c03399a41e46756bb6ab2f31a88288177e
Jonas Angelo Clamor <jonasangelo.clamor@cit.edu>
2022-11-22 16:58:48 +0800

    Implement insertAssessment Method In AssessmentService

--- gamify/src/main/java/com/teknocrats/gamify/Service/AssessmentService.java
+++ gamify/src/main/java/com/teknocrats/gamify/Service/AssessmentService.java
@@ -1,5 +1,29 @@
 package com.teknocrats.gamify.Service;
 
-public class AssessmentService {
+import java.util.List;
+import java.util.NoSuchElementException;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.teknocrats.gamify.Entity.AssessmentEntity;
+import com.teknocrats.gamify.Repository.AssessmentRepository;
+
+@Service
+public class AssessmentService {
+	
+	/* contains all the business logic of the app
+	 * Create an assessment record
+	 * Read assessment by assessmentid or title
+	 * Update an assessment record
+	 * Delete an assessment record
+	 **/
+	
+	@Autowired
+	AssessmentRepository assessmentRepository;
+	
+		//Create or insert an assessment record 
+		public AssessmentEntity insertAssessment(AssessmentEntity assessment) {
+			return assessmentRepository.save(assessment);
+		}
 }
