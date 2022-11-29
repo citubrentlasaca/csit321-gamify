3edd084d49d3f6ef6db380c75e126504f7d372d7
Stephanie Micah Cahutay <stephaniemicah.cahutay@cit.edu>
2022-11-29 14:03:10 +0800

    Add getByTeacherid method

--- gamify/src/main/java/com/teknocrats/gamify/Repository/TeacherRepository.java
+++ gamify/src/main/java/com/teknocrats/gamify/Repository/TeacherRepository.java
@@ -2,10 +2,11 @@ package com.teknocrats.gamify.Repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
- 
+
+import com.teknocrats.gamify.Entity.ItemEntity;
 import com.teknocrats.gamify.Entity.TeacherEntity;
 
 @Repository
 public interface TeacherRepository extends JpaRepository<TeacherEntity, Integer>{
-
+	TeacherEntity findByTeacherid(int teacherid);
 }
