2241e0d38514176da4922d651d049939f6078de3
Elaiza Faye Pepito <elaizafaye.pepito@cit.edu>
2022-12-20 15:29:25 +0800

    Modify Price

--- gamify-app/src/Components/Pepito/Price.js
+++ gamify-app/src/Components/Pepito/Price.js
@@ -1,10 +1,10 @@
-import logo from '../../images/logo.png';
+import logo from '../../Images/Pepito/logo.png';
 import * as React from 'react';
 import { Button, Stack, Tabs, Tab, Menu, MenuItem, IconButton, ListItemIcon, ListItemText, Divider } from "@mui/material";
 import MenuIcon from '@mui/icons-material/Menu';
 import LogoutIcon from '@mui/icons-material/Logout';
 import PopupState, { bindTrigger, bindMenu } from 'material-ui-popup-state';
-import brand from '../../images/brand.png';
+import brand from '../../Images/Pepito/brand.png';
 
 
 
