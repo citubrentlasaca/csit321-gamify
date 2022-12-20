d7e5a99f607493df48b6f125aa6c91b4d929466c
Elaiza Faye Pepito <elaizafaye.pepito@cit.edu>
2022-12-20 15:34:41 +0800

    Update App.js

--- gamify-app/src/App.js
+++ gamify-app/src/App.js
@@ -2,13 +2,25 @@ import './App.css';
 import * as React from 'react';
 import AddItems from './Components/Lasaca/AddItems.js'
 import AnswerAssessment from './Components/Lasaca/AnswerAssessment.js';
-import AboutUs from './Components/Pepito/AboutUs.js';
+import StudentResult from './Components/Pepito/StudentResult';
+import StudentReview from './Components/Pepito/StudentReview';
+import TeacherResult from './Components/Pepito/TeacherResult';
+import TeacherDownloadResult from './Components/Pepito/TeacherDownloadResult';
+import Price from './Components/Pepito/Price';
+import AboutUs from './Components/Pepito/AboutUs';
+import FAQ from './Components/Pepito/FAQ';
 import {Stack} from "@mui/material";
 
 function App() {
   return (
     <div>
         <AboutUs></AboutUs>
+        <Price></Price>
+        <FAQ></FAQ>
+        <StudentResult></StudentResult>
+        <StudentReview></StudentReview>
+        <TeacherResult></TeacherResult>
+        <TeacherDownloadResult></TeacherDownloadResult>
     </div>
 
     /* 
