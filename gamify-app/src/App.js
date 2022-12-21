1d7a381c3157e6d13c6716288353b9d7d8da4481
Ginalyn Caneda <ginalyn.caneda@cit.edu>
2022-12-21 12:27:08 +0800

    Add Route

--- gamify-app/src/App.js
+++ gamify-app/src/App.js
@@ -3,9 +3,9 @@ import * as React from 'react';
 import AddItems from './Components/Lasaca/AddItems.js'
 import AnswerAssessment from './Components/Lasaca/AnswerAssessment.js';
 import Landing from './Components/ginalyn/Landing.js';
-//import SetUpAccount from './Components/ginalyn/SetUpAccount.js';
+import SetUpAccount from './Components/ginalyn/SetUpAccount.js';
 import RegistrationFormStudent from './Components/ginalyn/RegistrationFormStudent.js';
-//import ChooseAvatar from './Components/ginalyn/ChooseAvatar.js';
+import ChooseAvatar from './Components/ginalyn/ChooseAvatar.js';
 import Homepage from './Components/ginalyn/Homepage.js';
 import ActivityStudent from './Components/ginalyn/ActivityStudent.js';
 import {Stack} from "@mui/material";
@@ -17,12 +17,22 @@ import CahutayManageTeachersProfilePage1 from './Components/Cahutay/CahutayManag
 import CahutayManageTeachersProfilePage2 from './Components/Cahutay/CahutayManageTeachersProfilePage2';
 import CahutayDeactivateTeacherAccount from './Components/Cahutay/CahutayDeactivateTeacherAccount';
 import CahutayDeactivateTeacherAccountConfirmation from './Components/Cahutay/CahutayDeactivateTeacherAccountConfirmation';
-import SetUpAccount from './Components/Cahutay/SetupAccount';
+import CahutaySetUpAccount from './Components/Cahutay/SetupAccount';
 import RegistrationFormTeacher from './Components/Cahutay/RegistrationFormTeacher';
-import ChooseAvatar from './Components/Cahutay/ChooseAvatar';
+import CahutayChooseAvatar from './Components/Cahutay/ChooseAvatar';
 function App() {
   return(
-    <div classname="App">
+    <Routes>
+      <Route path="" element={<Landing />}></Route>
+      <Route path="register" element={<CahutayChooseAccount />}></Route>
+      <Route path="register-student" element={<RegistrationFormStudent />}></Route>
+      <Route path="register-choose-student-avatar" element={<ChooseAvatar />}></Route>
+      <Route path="register-setup-student-account" element={<SetUpAccount />}></Route>
+      <Route path="register-setup-teacher-account" element={<CahutaySetUpAccount />}></Route>
+      <Route path="register-choose-student-avatar" element={<CahutayChooseAvatar />}></Route>
+      <Route path="*" element={<Navigate to="/" replace />}/>
+    </Routes>
+    /*<div classname="App">
     <header className="App-header2-gc">
         <CahutayChooseAccount></CahutayChooseAccount>
         <CahutayLoginAccountForm></CahutayLoginAccountForm>
@@ -36,19 +46,13 @@ function App() {
         <ChooseAvatar></ChooseAvatar>
       </header>
     </div>
-   /* <Stack>
+    <Stack>
        <AddItems/>
         
          <AnswerAssessment/>
     </Stack> 
 
-    <Routes>
-      <Route path="" element={<Landing />}></Route>
-      <Route path="register" element={<SetUpAccount />}></Route>
-      <Route path="register-student" element={<RegistrationFormStudent />}></Route>
-      <Route path="register-choose-avatar" element={<ChooseAvatar />}></Route>
-      <Route path="*" element={<Navigate to="/" replace />}/>
-    </Routes>*/
+    */
     /*<div>
       <Landing></Landing>
       <SetUpAccount></SetUpAccount>
